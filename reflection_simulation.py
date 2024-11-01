4113d5ff0bbfb4d8d3f18c18e3df8b1a9c87f30f
Andrew Elwood <amelwood01@gmail.com>
2024-11-01 21:06:55 +0000

    style.css added and updated into index.html

--- reflection_simulation.py
+++ reflection_simulation.py
@@ -1,2 +1,11 @@
 """A basic simulation of light reflecting off a mirror as the first sub-project of this
 project."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+# Initial parameters
+initial_angle = 30  # Angle of incidence in degrees
+mirror_x = np.array([-1, 1])  # Mirror surface along the x-axis
+mirror_y = np.array([0, 0])
